9c7260a3559e408983a57c8beefacb95cbf4a575
gabrielsanttana <gabriel.gsantana7@gmail.com>
2020-09-30 22:54:17 -0300

    Add simple set of exercises

---
+++ src/exercises/01.sql
@@ -0,0 +1,25 @@
+-- Average salary of the company:
+select avg(salary)
+from salaries s 
+
+-- Youngest person in the company:
+select max(birth_date)
+from employees e 
+
+-- Number of towns in Frace
+select count(id)
+from towns 
+
+-- Number of official languages in the world
+select count(countrycode)
+from countrylanguage c
+where isofficial = true
+
+-- Average life expectancy in the world:
+select avg(lifeexpectancy)
+from country c 
+
+-- Average population for cities in the Netherlands:
+select avg(population)
+from city c 
+where countrycode = 'NLD'
