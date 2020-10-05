b61f74e400752d2c7c2a765d6fe841b798aa0dcb
Gabriel Santana <gabriel.gsantana7@gmail.com>
2020-10-05 14:54:01 -0300

    Update 01.sql

--- src/exercises/01.sql
+++ src/exercises/01.sql
@@ -1,25 +1,25 @@
 -- Average salary of the company:
 select avg(salary)
-from salaries s 
+from salaries s;
 
 -- Youngest person in the company:
 select max(birth_date)
-from employees e 
+from employees e;
 
 -- Number of towns in Frace
 select count(id)
-from towns 
+from towns;
 
 -- Number of official languages in the world
 select count(countrycode)
 from countrylanguage c
-where isofficial = true
+where isofficial = true;
 
 -- Average life expectancy in the world:
 select avg(lifeexpectancy)
-from country c 
+from country c;
 
 -- Average population for cities in the Netherlands:
 select avg(population)
 from city c 
-where countrycode = 'NLD'
+where countrycode = 'NLD';
